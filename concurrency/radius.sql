f465078919f0bebc4a3644b86b47b34dceff0914
Boan Tua Pasaribu <boantuapasaribu@gmail.com>
2018-09-22 20:10:05 +0700

    Update radius.sql

--- concurrency/radius.sql
+++ concurrency/radius.sql
@@ -1,16 +1,1 @@
-SELECT 
-  ROUND(
-    6371 * ACOS(
-      SIN(-6.597147 * PI() / 180) * SIN(restaurant_latitude * PI() / 180) + COS(-6.597147 * PI() / 180) * COS(restaurant_latitude * PI() / 180) * COS(
-        (restaurant_longitude * PI() / 180) - (106.806038 * PI() / 180)
-      )
-    ),
-    1
-  ) AS distance,
-  restaurant_name,
-  restaurant_location,
-  restaurant_latitude,
-  restaurant_longitude 
-FROM
-  restaurant 
-ORDER BY distance ASC 
+SELECT ROUND( 6371 * ACOS( SIN(- 6.597147 * PI() / 180) * SIN(restaurant_latitude * PI() / 180) + COS(- 6.597147 * PI() / 180) * COS(restaurant_latitude * PI() / 180) * COS( (restaurant_longitude * PI() / 180) - (106.806038 * PI() / 180) ) ), 1 ) AS distance, restaurant_name, restaurant_location, restaurant_latitude, restaurant_longitude FROM restaurant ORDER BY distance ASC 
